a6364ed56973dfd2f23d57baf33cb1192ac9cad0
Mohamed Emara <mohamedemara33@gmail.com>
2026-06-11 16:52:18 +0300

    style(psr/simple-cache): fix code formatting
    condense empty interface declarations and standardize indentation across all interface files, with no functional changes

--- vendor/psr/simple-cache/src/CacheException.php
+++ vendor/psr/simple-cache/src/CacheException.php
@@ -5,6 +5,4 @@ namespace Psr\SimpleCache;
 /**
  * Interface used for all types of exceptions thrown by the implementing library.
  */
-interface CacheException extends \Throwable
-{
-}
+interface CacheException extends \Throwable {}
